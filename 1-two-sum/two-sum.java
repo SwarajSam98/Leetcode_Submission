90ea716bbbd816a0e3e5595c15bfa0d2cae03974
SwarajSam <68324901+SwarajSam98@users.noreply.github.com>
2025-05-22 11:01:55 -0400

    Time: 2 ms (98.84%) | Memory: 45.3 MB (10.13%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> num_map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int compli = target - nums[i];
+            if (num_map.containsKey(compli)){
+                return new int[]{num_map.get(compli), i};
+            }
+            num_map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No Mastcvh found");
+
+        
+    }
+}
