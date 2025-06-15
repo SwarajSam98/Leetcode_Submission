dc32544cf6d65efdb49fc37ba9a0ed6a00c37c1c
SwarajSam <68324901+SwarajSam98@users.noreply.github.com>
2025-06-15 16:31:58 -0400

    Time: 3 ms (84.22%) | Memory: 49.3 MB (85.19%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,36 +1,27 @@
 class Solution {
     public int numIslands(char[][] grid) {
-         int ans = 0;
+    int ans = 0;
 
     for (int i = 0; i < grid.length; ++i)
       for (int j = 0; j < grid[0].length; ++j)
         if (grid[i][j] == '1') {
-          bfs(grid, i, j);
+          dfs(grid, i, j);
           ++ans;
         }
 
     return ans;
   }
 
-  private static final int[][] DIRS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+  private void dfs(char[][] grid, int i, int j) {
+    if (i < 0 || i == grid.length || j < 0 || j == grid[0].length)
+      return;
+    if (grid[i][j] != '1')
+      return;
 
-  private void bfs(char[][] grid, int r, int c) {
-    Queue<Pair<Integer, Integer>> q = new ArrayDeque<>(List.of(new Pair<>(r, c)));
-    grid[r][c] = '2'; // Mark '2' as visited.
-    while (!q.isEmpty()) {
-      final int i = q.peek().getKey();
-      final int j = q.poll().getValue();
-      for (int[] dir : DIRS) {
-        final int x = i + dir[0];
-        final int y = j + dir[1];
-        if (x < 0 || x == grid.length || y < 0 || y == grid[0].length)
-          continue;
-        if (grid[x][y] != '1')
-          continue;
-        q.offer(new Pair<>(x, y));
-        grid[x][y] = '2'; // Mark '2' as visited.
-      }
-    }
-        
+    grid[i][j] = '2'; // Mark '2' as visited.
+    dfs(grid, i + 1, j);
+    dfs(grid, i - 1, j);
+    dfs(grid, i, j + 1);
+    dfs(grid, i, j - 1);
     }
 }
