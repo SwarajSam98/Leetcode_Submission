96b2eda79c6e03b99e490aa19f03129d51d5206b
SwarajSam <68324901+SwarajSam98@users.noreply.github.com>
2025-08-15 22:05:01 -0400

    Time: 0 ms (100.00%) | Memory: 44.9 MB (32.87%) - LeetSync

---
+++ 772-construct-quad-tree/construct-quad-tree.java
@@ -0,0 +1,62 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+    public Node construct(int[][] grid) {
+        return helper(grid, 0, 0, grid.length);
+        
+    }
+    private Node helper(int[][] grid, int i, int j, int w){
+        if(allSame(grid, i, j, w))
+            return new Node(grid[i][j] == 1, true);
+        final int half = w / 2;
+        return new Node(true, false, helper(grid, i, j, half),
+                            helper(grid, i, j + half, half),
+                            helper(grid, i + half, j, half),
+                            helper(grid, i + half, j+ half, half));
+    }
+    private boolean allSame(int[][] grid, int i, int j, int w){
+        for(int x = i; x < i + w; ++x)
+            for(int y =j; y < j + w; ++y)
+                if(grid[x][y] != grid[i][j])
+                    return false;
+        return true;
+    }
+}
