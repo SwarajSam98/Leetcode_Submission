07aab48aa77907232bda94905264b9f3b7eca7df
SwarajSam <68324901+SwarajSam98@users.noreply.github.com>
2025-09-29 19:41:48 -0400

    Time: 6 ms (9.03%) | Memory: 41.7 MB (17.13%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
@@ -1,33 +1,15 @@
 class Solution {
-
-    int n;
-    int[] values;
-    Map<Integer, Integer> memo = new HashMap<Integer, Integer>();
-
     public int minScoreTriangulation(int[] values) {
-        this.n = values.length;
-        this.values = values;
-        return dp(0, n - 1);
-    }
-
-    public int dp(int i, int j) {
-        if (i + 2 > j) {
-            return 0;
-        }
-        if (i + 2 == j) {
-            return values[i] * values[i + 1] * values[j];
-        }
-        int key = i * n + j;
-        if (!memo.containsKey(key)) {
-            int minScore = Integer.MAX_VALUE;
-            for (int k = i + 1; k < j; k++) {
-                minScore = Math.min(
-                    minScore,
-                    values[i] * values[k] * values[j] + dp(i, k) + dp(k, j)
-                );
+        int n = values.length;
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int k = i + 1; k < j; ++k) {
+                    dp[i][j] = Math.min(dp[i][j] == 0 ? Integer.MAX_VALUE : dp[i][j],
+                        dp[i][k] + values[i] * values[k] * values[j] + dp[k][j]);
+                }
             }
-            memo.put(key, minScore);
         }
-        return memo.get(key);
+        return dp[0][n - 1];
     }
 }
